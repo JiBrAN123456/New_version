f432d5f28ce5c6d4f0719e11c18c930c2ee4ba11
Mohammad Jibran <yunisquadri99@gmail.com>
2025-04-17 03:16:18 +0530

    first model made

--- invoices/models.py
+++ invoices/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from inventory.models import VehicleInventory
+from users.models import Company
+import uuid
 
-# Create your models here.
+
+class Transaction(models.Model):
+    TRANSACTION_TYPE_CHOICES = [
+        ("Sale","SALE"),
+        ("Purchase","PURCHASE"),
+    ]
+
+    vehicle = models.ForeignKey(VehicleInventory, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company , on_delete=models.CASCADE)
+    buyer_name = models.CharField(max_length=255 , null = True , blank= True)
+    seller_name = models.CharField(max_length=255 , null = True , blank= True)
+    transaction_type = models.CharField( max_length= 20 , choices=TRANSACTION_TYPE_CHOICES)
+    price = models.DecimalField(max_digits=12 , decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+    notes = models.TextField(blank=True , null=True)
+
+    def __str__(self):
+        return f"{self.transaction_type.title()} - {self.vehicle}"
+    
+
+
+
+class Invoice(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4 , editable=False)
+    transaction = models.OneToOneField(Transaction, on_delete= models.CASCADE)
+    invoice_number = models.CharField(max_length=100 , unique=True)
+    generated_on = models.DateTimeField(auto_now_add=True)
+    pdf_file = models.FileField(upload_to="invoices/", blank=True , null = True)
+
+
+    def __str__(self):
+        return f"invoice number is {self.invoice_number}"    
